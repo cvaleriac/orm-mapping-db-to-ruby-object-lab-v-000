a1bb5a97a54457cd0d9def1a4c145e5b686d1399
valeria campilongo <cvaleriac@hotmail.it>
2018-08-31 20:27:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,6 +56,37 @@ class Student
     end
   end
 
+  def self.first_x_students_in_grade_10(x)
+    arr = []
+    self.all.select do |student|
+      arr << student.grade == 10
+    end
+    arr.take(x)
+    # sql = <<-SQL
+    #   SELECT * FROM students WHERE grade = 10 ORDER BY grade limit ?;
+    # SQL
+    #
+    # DB[:conn].execute(sql)
+  end
+   def self.first_student_in_grade_10
+    # student = self.all.find do |student|
+    #   student.grade == 10
+    # end
+    # student
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 limit 1;
+    SQL
+     rows = DB[:conn].execute(sql)
+    self.new_from_db(rows.first)
+  end
+   def self.all_students_in_grade_X(x)
+    arr = []
+    self.all.select do |student|
+      arr << student.grade == x
+    end
+    arr
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
